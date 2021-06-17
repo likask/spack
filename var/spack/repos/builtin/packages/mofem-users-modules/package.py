076ac5fb6b829de43d8a4b4d0801d260f3f2f7de
Lukasz Kaczmarczyk <lukasz.kaczmarczyk@glasgow.ac.uk>
2021-06-17 10:07:56 +0100

    bump version
    (cherry picked from commit 63ee669fccbe856370970d9e2b2c7150fc5133af)

--- var/spack/repos/builtin/packages/mofem-users-modules/package.py
+++ var/spack/repos/builtin/packages/mofem-users-modules/package.py
@@ -18,6 +18,7 @@ class MofemUsersModules(CMakePackage):
 
     version('develop', branch='develop')
     version('lukasz', branch='lukasz/develop')
+    version('0.12.2', branch='Version0.12.2')
     version('0.12.1', branch='Version0.12.1')
     version('0.12.0', branch='Version0.12.0')
     version('0.11.1', branch='Version0.11.1')
@@ -49,6 +50,7 @@ class MofemUsersModules(CMakePackage):
         description='Copy user modules directory instead linking')
 
     extends('mofem-cephas')
+    depends_on('mofem-cephas@0.12.1', when='@0.12.2')
     depends_on('mofem-cephas@0.12.0', when='@0.12.1')
     depends_on('mofem-cephas@0.12.0', when='@0.12.0')
     depends_on('mofem-cephas@0.11.1', when='@0.11.1')
